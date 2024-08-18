d222754db6f8b3ae4b365f70f6696a6b7574a17c
Daniel Smith <danoz73@gmail.com>
2024-08-18 01:42:15 -0700

    Updated the tutorial

--- src/components/widgets/Tutorial.svelte
+++ src/components/widgets/Tutorial.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <h3>how to play</h3>
-<div>Guess the <strong>WORDLE</strong> in {ROWS} tries.</div>
-<div>Each guess must be a valid {COLS} letter word. Hit the enter button to submit.</div>
+<div>Guess the name of our baby girl in {ROWS} tries!</div>
+<div>Each guess must be a {COLS} letter guess. Hit the enter button to submit.</div>
 <div>
 	After each guess, the color of the tiles will change to show how close your guess was to the
 	word.
@@ -15,42 +15,32 @@
 <div class:complete={visible} class="examples">
 	<div><strong>Examples</strong></div>
 	<div class="row">
-		<Tile value="w" state="🟩" />
-		<Tile value="e" state="🔳" />
+		<Tile value="i" state="🟩" />
+		<Tile value="l" state="🔳" />
 		<Tile value="a" state="🔳" />
-		<Tile value="r" state="🔳" />
-		<Tile value="y" state="🔳" />
+		<Tile value="n" state="🔳" />
+		<Tile value="a" state="🔳" />
 	</div>
-	<div>The letter <strong>W</strong> is in the word and in the correct spot.</div>
+	<div>The letter <strong>I</strong> is in the name and in the correct spot.</div>
 	<div class="row">
-		<Tile value="p" state="🔳" />
-		<Tile value="i" state="🟨" />
-		<Tile value="l" state="🔳" />
-		<Tile value="l" state="🔳" />
-		<Tile value="s" state="🔳" />
-	</div>
-	<div>The letter <strong>I</strong> is in the word but in the wrong spot.</div>
-	<div class="row">
+		<Tile value="a" state="🔳" />
+		<Tile value="v" state="🟨" />
+		<Tile value="i" state="🔳" />
 		<Tile value="v" state="🔳" />
 		<Tile value="a" state="🔳" />
-		<Tile value="g" state="🔳" />
-		<Tile value="u" state="⬛" />
-		<Tile value="e" state="🔳" />
 	</div>
-	<div>The letter <strong>U</strong> is not in the word in any spot.</div>
+	<div>The letter <strong>V</strong> is in the name but in the wrong spot.</div>
+	<div class="row">
+		<Tile value="n" state="🔳" />
+		<Tile value="a" state="🔳" />
+		<Tile value="o" state="🔳" />
+		<Tile value="m" state="⬛" />
+		<Tile value="i" state="🔳" />
+	</div>
+	<div>The letter <strong>M</strong> is not in the name in any spot.</div>
 </div>
 <div>
-	This is a recreation of the original <a
-		href="https://www.nytimes.com/games/wordle/"
-		target="_blank"
-		rel="noreferrer">Wordle</a
-	>
-	by Josh Wardle with additional modes and features, allowing you to play infinite wordles. Switch
-	to infinite mode to play an unlimited number of times.
-	<br /><br />
-	Open the settings menu to see some of the additional features.
-	<br />
-	Written with Svelte, in Typescript by
+	Based on the open source code written by
 	<a href="https://github.com/MikhaD" target="_blank" rel="noreferrer">MikhaD</a>.
 </div>
 
